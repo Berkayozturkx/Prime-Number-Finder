02683504e7fa4b06589338a4d975dd59a38b88a3
Berkay Ozturk <berkay_ozturk0151@hotmail.com>
2023-06-25 17:04:37 +0300

    Prime Number Finder on Python with PyQt5 interface

---
+++ Prime-Number-Finder.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+import sys
+
+class pencere(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.init_ui()
+        
+    def init_ui(self):
+        self.setWindowTitle("Asal Sayı Bulucu")
+        
+        grid = QGridLayout()
+
+        grid.addWidget(QLabel("Sayı Giriniz:"),1,0)
+        
+        self.sayi = QLineEdit()
+        grid.addWidget(self.sayi,1,1,1,2)
+        
+        self.buton = QPushButton("Bul")
+        grid.addWidget(self.buton,2,1,1,2)
+        self.buton.clicked.connect(self.hesapla)
+        
+        self.yazi = QLabel("")
+        grid.addWidget(self.yazi,4,1)
+        
+        self.setLayout(grid)
+        self.resize(300,100)
+        self.show()
+        
+    def hesapla(self):
+        sayi = 0
+        try:
+            sayi = int(self.sayi.text())
+        except:
+            pass
+        sayac = 0
+        for i in range(2,sayi):
+            if(sayi % i == 0):
+                sayac += 1
+        if(sayac == 0):
+            self.yazi.setText(str(sayi) + " sayısı asaldır.")
+        else:
+            self.yazi.setText(str(sayi) + " sayısı asal değildir.")
+uygulama = QApplication(sys.argv)
+pencere = pencere()
+sys.exit(uygulama.exec_())
+
+
+# In[ ]:
+
+
+
+
---
+++ README.md
@@ -0,0 +1,28 @@
+# Asal Sayı Bulucu / Prime Number Finder
+
+---
+
+![asal_sayi_bulucu](https://github.com/YazilimciAmaAcemiOlan/Prime-Number-Finder/assets/127662417/ffbc7202-fd2c-4d92-b6b3-1aa1ac001d8e)
+
+
+[TR]
+Programın arayüzü yukarıda gözüktüğü gibidir. Kullanımı ise son derece kolaydır. Kullanıcının asal olup olmadığını sorgulamak istediği sayıyı girdikten sonra "Bul" butonuna tıklaması yeterlidir.
+
+[EN]
+The interface of the program is as shown above. It is very easy to use. It is enough for the user to click on the "Bul" button after entering the number they want to query whether it is prime or not.
+
+## Örnekler / Examples
+
+---
+![ornek](https://github.com/YazilimciAmaAcemiOlan/Prime-Number-Finder/assets/127662417/a3eb3860-6546-428f-a056-ae689db6f3d4)
+
+![ornek1](https://github.com/YazilimciAmaAcemiOlan/Prime-Number-Finder/assets/127662417/086beba3-e9c2-47df-87ae-fcfa8a624ab7)
+
+---
+
+### Bilgilendirme / Information
+[TR]
+Bu uygulamanın yapımında PyQt5 kütüphanesi kullanılmıştır.
+
+[EN]
+PyQt5 library was used to build this application.
